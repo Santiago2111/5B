6ab809241ae3e8debc114d238ba75c8d43392ac3
Santiago <Santiago@gmail.com>
2023-03-31 10:09:08 -0500

    Update

---
+++ examples/add.php
@@ -0,0 +1,9 @@
+<?php
+    $n1=10;
+    $n2=20;
+    $answer=$n1+$n2;
+    echo "<b>The sum is: </b>".$n1+$n2."<br>";
+    echo "<b>The subtraction is: </b>".$n1-$n2."<br>";
+    echo "<b>The multiplication is: </b>".$n1*$n2."<br>";
+    echo "<b>The division is: </b>".$n1/$n2."<br>";
+?>    
---
+++ examples/hello.php
@@ -0,0 +1,9 @@
+<?php
+    //This is my first php file
+    /*
+        Developer: Santiago
+        Date: 28-03-2023
+        Version: 1.0
+    */
+    echo "Hello, this is my php file";
+?>
